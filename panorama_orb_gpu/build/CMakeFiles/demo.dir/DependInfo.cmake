5434005b2360febb4c6e3ce8d057c4b7a4fba65e
mjlin <mjlin@nullmax.ai>
2018-01-05 18:10:27 +0800

    add_gpu_orb_extrator

---
+++ panorama_orb_gpu/build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ustc/panorama_orb_gpu/src/ORBextractor.cc" "/home/ustc/panorama_orb_gpu/build/CMakeFiles/demo.dir/src/ORBextractor.cc.o"
+  "/home/ustc/panorama_orb_gpu/src/panorama_orb_gpu_node.cpp" "/home/ustc/panorama_orb_gpu/build/CMakeFiles/demo.dir/src/panorama_orb_gpu_node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/ustc/nullmax/Pangolin/include"
+  "/home/ustc/nullmax/Pangolin/build/src/include"
+  "/usr/local/cuda-8.0/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ panorama_orb_gpu/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/src/cuda/demo_generated_Allocator_gpu.cu.o"
+  "CMakeFiles/demo.dir/src/cuda/demo_generated_Fast_gpu.cu.o"
+  "CMakeFiles/demo.dir/src/cuda/demo_generated_Orb_gpu.cu.o"
+  "CMakeFiles/demo.dir/src/cuda/demo_generated_Cuda.cu.o"
+  "CMakeFiles/demo.dir/src/ORBextractor.cc.o"
+  "CMakeFiles/demo.dir/src/panorama_orb_gpu_node.cpp.o"
+  "demo.pdb"
+  "demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
